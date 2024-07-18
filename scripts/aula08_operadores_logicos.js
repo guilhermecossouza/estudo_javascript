033cdb2b0fc2ac8de702a28b72e27c64763adad6
guilhermecossouza@gmail.com <guilhermecossouza@gmail.com>
2024-07-18 08:25:50 -0300

    operadores lógicos

---
+++ scripts/aula08_operadores_logicos.js
@@ -0,0 +1,24 @@
+/** Operadores lógicos
+ *  && -> and e
+ *  || -> or ou
+ *  ! -> not não  
+ * 
+ *  TABLEA && -> AND E
+ *  V && V = V
+ *  V && F = F
+ *  F && V = F
+ *  F && F = F
+ * 
+ * TABELA || -< OR OU
+ *  V || V = V
+ *  V || F = V 
+ *  F || V = V
+ *  F || F = F   
+ */
+
+let n1 = 10
+let n3 = 5
+let n4 = 15
+let n5 = 2
+
+console.log((n1 > n2) && (n1 > n3))
