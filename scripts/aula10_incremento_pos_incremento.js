a65e9a496a3435ef8b887b5f87c71546fd97657f
guilhermecossouza@gmail.com <guilhermecossouza@gmail.com>
2024-07-22 08:53:42 -0300

    Aula de operadores

---
+++ scripts/aula10_incremento_pos_incremento.js
@@ -0,0 +1,36 @@
+"use strict"
+//OBS: ESSA OPERAçãO CHAMA-SE PóS INCREMENTO 
+// o java script pega o valor da variável e dpois incrementa
+
+console.log("ESSA OPERAÇÃO CHAMA-SE PÓS INCREMENTO");
+let n = 10;
+n++
+console.log(n); // resultado é 11/
+
+let n2 = 10;
+console.log(n2++); // reultado aqui é 10
+console.log(n2); // resultado aqui é 11 porque em cima ele console.logou 10 e depois incrementou
+
+
+//PRÉ INCREMENTO
+console.log("PRÉ INCREMENTO");
+let n3 = 10
+++n3
+console.log(n3); // resultado é 11
+
+let n4 = 10
+console.log(++n4); // resultado é 11 ou senha ele var pegar o incremento e somar a variável
+
+console.log("")
+//INVERSSÃO DE VALORES
+console.log("INVERSSÃO DE VALORES")
+let n5 = 10 // valor positivo
+console.log(n5);
+let n6 = -10 // inverteu o número positivo para negativo
+console.log(n6);
+let n7 = +10 //inverteu o valor negativo para positivo
+console.log(n7);
+
+
+
+
